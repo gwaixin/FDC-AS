6b55c0a3ca13b6853c4298203b7130385178bf29
John Mart Belamide <johnmart_belamide@yahoo.com>
2015-05-13 16:16:45 +0800

    2nd update
    Edit, validate, search employee

--- app/Controller/EmployeesController.php
+++ app/Controller/EmployeesController.php
@@ -34,37 +34,121 @@ class EmployeesController extends AppController {
 												),
 											array(
 													'table' => 'contract_logs',
+													'type' => 'LEFT',
 													'conditions' => array(
-															'employee.current_contract_id = contract_logs.id'
+															'employee.id = contract_logs.employees_id'
 													)
 												)
 											);
+			$conditions = array("concat(profiles.first_name, ' ',profiles.middle_name,' ',profiles.last_name) LIKE '%" . $this->request->data['value'] . "%'");
+			switch($this->request->data['field']) {
+				case "name":
+					$conditions = array("concat(profiles.first_name, ' ',profiles.middle_name,' ',profiles.last_name) LIKE '%" . $this->request->data['value'] . "%'");
+				break;
+				case "employee_id":
+					$conditions = array("employee_id LIKE '%" . $this->request->data['value'] . "%'");
+				break;
+				case "position":
+					if($this->request->data['value']) {
+						$positionLevelCondition = "";
+						if($this->request->data['position_level']) {
+							$positionLevelCondition = "and position_levels.description = '" . $this->request->data['position_level'] . "'";
+						}
+						$conditions = array("positions.description = '" . $this->request->data['value'] . "' $positionLevelCondition");
+					}
+				break;
+				case "status":
+					$conditions = array("employee.status = '" . $this->request->data['value'] . "'");
+				break;
+			}
 			$employees = $this->Employee->find('all',array(
 																						'joins' => $joins,
+																						'conditions' => $conditions,
 																						'fields' => array('*')
 																						)
 																				);
 			$employees_arr = [];
 			foreach($employees as $key => $employee) {
 				$data = array(
-										'id' => $employee['Employee']['profile_id'],
-										'employee_id' => $employee['Employee']['id'],
+										'id' => $employee['Employee']['id'],
 										'name' => $employee['profiles']['first_name']. " " . $employee['profiles']['middle_name'] . " " .$employee['profiles']['last_name'],
+										'employee_id' => $employee['Employee']['employee_id'],
 										'tin' => $employee['Employee']['tin'],
 										'salary' => $employee['Employee']['salary'],
 										'drug_test' => $employee['Employee']['drug_test'],
 										'pagibig' => $employee['Employee']['pagibig'],
 										'philhealth' => $employee['Employee']['philhealth'],
+										'medical' => $employee['Employee']['medical'],
 										'sss' => $employee['Employee']['sss'],
 										'insurance_id' => $employee['Employee']['insurance_id'],
 										'position' => $employee['positions']['description'],
 										'position_level' => $employee['position_levels']['description'],
 										'contract' => $employee['contract_logs']['description'],
+										'first_time_in' => $employee['Employee']['f_time_in'],
+										'first_time_out' => $employee['Employee']['f_time_out'],
+										'last_time_in' => $employee['Employee']['l_time_in'],
+										'last_time_out' => $employee['Employee']['l_time_out'],
+										'role' => $employee['Employee']['role'],
+										'status' => ($employee['Employee']['status']) ? "Active" : "Inactive"
+									);
+				array_push($employees_arr,$data);
+			}
+			if(!$employees_arr) {
+				$data = array(
+										'id' => null,
+										'employee_id' => null,
+										'name' => null,
+										'tin' => null,
+										'salary' => null,
+										'drug_test' => null,
+										'pagibig' => null,
+										'philhealth' => null,
+										'medical' => null,
+										'sss' => null,
+										'insurance_id' => null,
+										'position' => null,
+										'position_level' => null,
+										'contract' => null,
+										'first_time_in' => null,
+										'first_time_out' => null,
+										'last_time_in' => null,
+										'last_time_out' => null,
+										'role' => null,
+										'status' => null
 									);
 				array_push($employees_arr,$data);
 			}
 			echo json_encode($employees_arr);
 		}
+
+	}
+
+	public function getDropdownValues() {
+
+		if($this->request->is('ajax')) {
+			$this->autoRender = false;
+			$json['names'] = $this->suggestNameLists();
+			$json['positions'] = $this->getPositionLists();
+			$json['positionLevels'] = $this->getPositionLevelLists();
+			echo json_encode($json);
+		}
+
+	}
+
+	public function suggestNameLists() {
+
+		$this->autoRender = false;
+		$this->loadModel('Profiles');
+		$employees = $this->Profiles->find('all',array(
+																								'conditions' => array("id not in (Select profile_id from employees)")
+																							)
+																						);
+		$names = [];
+		foreach($employees as $employee) {
+			$name =  $employee['Profiles']['first_name'] . " " . $employee['Profiles']['middle_name'] . " " . $employee['Profiles']['last_name'];
+			array_push($names,$name);
+		}
+		return $names;
 
 	}
 
@@ -74,16 +158,83 @@ class EmployeesController extends AppController {
 			$this->autoRender = false;
 			$this->loadModel('Profiles');
 			$employees = $this->Profiles->find('all',array(
-																									'conditions' => array("concat(first_name,' ',middle_name,' ',last_name) LIKE '%".$this->request->data['name']."%' 
-																									 and id not in (Select profile_id from employees)")
+																									'conditions' => array("id not in (Select profile_id from employees)")
 																								)
 																							);
 			$names = [];
-			echo "<option value=''> Employees </option>";
 			foreach($employees as $employee) {
 				$name =  $employee['Profiles']['first_name'] . " " . $employee['Profiles']['middle_name'] . " " . $employee['Profiles']['last_name'];
-				echo "<option value='$name'> $name </option>";
+				array_push($names,$name);
 			}
+			return $names;
+		}
+
+	}
+
+	public function getPositionLists() {
+
+		$this->autoRender = false;
+		$this->loadModel('Position');
+		$positions = $this->Position->find('all');
+		$position_arr = [];
+		foreach($positions as $position) {
+			array_push($position_arr,$position['Position']['description']);
+		}
+		return $position_arr;
+
+	}
+
+	public function getPositions() {
+
+		if($this->request->is('ajax')) {
+			$this->autoRender = false;
+			$this->loadModel('Position');
+			$positions = $this->Position->find('all');
+			$position_arr = [];
+			foreach($positions as $position) {
+				array_push($position_arr,$position['Position']['description']);
+			}
+			return $position_arr;
+		}
+
+	}
+
+	public function getPositionLevelLists() {
+
+			$this->autoRender = false;
+			$this->loadModel('Position');
+			$this->loadModel('Position_level');
+			$level_arr = [];
+			$levels = $this->Position_level->find('all',array(
+																										'fields' => array('distinct(description)')
+																										)
+																									);
+			foreach($levels as $level) {
+				array_push($level_arr,$level['Position_level']['description']);
+			}
+			return $level_arr;
+
+	}
+
+	public function getPositionLevels() {
+
+		if($this->request->is('ajax')) {
+			$this->autoRender = false;
+			$this->loadModel('Position');
+			$this->loadModel('Position_level');
+			$level_arr = [];
+			$position = $this->Position->findByDescription($this->request->data['position']);
+			if($position) {
+				$levels = $this->Position_level->find('all',array(
+																												'conditions' => array("positions_id = '" . $position['Position']['id'] . "'"
+																																						)
+																													)
+																										);
+				foreach($levels as $level) {
+					array_push($level_arr,$level['Position_level']['description']);
+				}
+			}
+			echo json_encode($level_arr);
 		}
 
 	}
@@ -97,46 +248,62 @@ class EmployeesController extends AppController {
 			$this->loadModel('Position');
 			$this->loadModel('Position_level');
 			$employee = $this->request->data['employee'];
+			$action = "edit";
 			$position = "";
 			$searchPosition = $this->Position->findByDescription($employee['position']);
 			if($searchPosition) {
 				$position = $searchPosition['Position']['id'];
 			}
 			$position_level = "";
-			$searchPositionLevel = $this->Position_level->findByDescription($employee['position_level']);
+			$searchPositionLevel = $this->Position_level->find('first',array(
+																																	'conditions' => array("positions_id = '$position' and description = '" . $employee['position_level'] . "'")
+																																)
+																															);
 			if($searchPositionLevel) {
 				$position_level = $searchPositionLevel['Position_level']['id'];
 			}
-			$exists = $this->Employee->findByProfile_id($employee['id']);
-			if($exists) {
-				$this->Employee->id = $employee['employee_id'];
+			$errors = "";
+			$exists = $this->Employee->findById($employee['id']);
+				if($exists) {
+				$this->Employee->id = $employee['id'];
 				$data = array(
-								'id' => $employee['employee_id'],
+								'employee_id' => $employee['employee_id'],
 								'tin' => $employee['tin'],
 								'salary' => $employee['salary'],
 								'drug_test' => $employee['drug_test'],
 								'pagibig' => $employee['pagibig'],
 								'philhealth' => $employee['philhealth'],
+								'medical' => $employee['medical'],
 								'sss' => $employee['sss'],
 								'insurance_id' => $employee['insurance_id'],
 								'position_id' => $position,
 								'position_level_id' => $position_level,
-								'current_contract' => 1
+								'current_contract' => 'null',
+								'f_time_in' => $employee['first_time_in'],
+								'f_time_out' => $employee['first_time_out'],
+								'l_time_in' => $employee['last_time_in'],
+								'l_time_out' => $employee['last_time_out'],
+								'status' => ($employee['status'] === "Active") ? 1 : 0
 							);
-				$this->Employee->save($data);
+				if(!$this->Employee->save($data)) {
+					foreach($this->Employee->validationErrors as $error) {
+						$errors .= $error[0]."\n";
+					}
+				}
 			} else {
-				$employee_id = "";
+				$profile_id = "";
 				$employeeInfo = $this->Profile->find('first',array(
 																									'conditions' => array("concat(first_name,' ',middle_name,' ',last_name) LIKE '%".$employee['name']."%'")
 																								)
 																							);
 				
 				if($employeeInfo) {
-					$employee_id = $employeeInfo['Profile']['id'];
+					$profile_id = $employeeInfo['Profile']['id'];
 				}
 				$data = array(
-							'id' => $employee['employee_id'],
-							'profile_id' => $employee_id,
+							'id' => $employee['id'],
+							'employee_id' => $employee['employee_id'],
+							'profile_id' => $profile_id,
 							'tin' => $employee['tin'],
 							'salary' => $employee['salary'],
 							'drug_test' => $employee['drug_test'],
@@ -144,13 +311,20 @@ class EmployeesController extends AppController {
 							'philhealth' => $employee['philhealth'],
 							'sss' => $employee['sss'],
 							'insurance_id' => $employee['insurance_id'],
-							'position_id' => $position['id'],
-							'position_level_id' => $position_level['id'],
-							'current_contract' => 1
+							'position_id' => $position,
+							'position_level_id' => $position_level
 						);
-				print_r($data);
-				//$this->Employee->save($data);
+				if(!$this->Employee->save($data)) {
+					foreach($this->Employee->validationErrors as $error) {
+						$errors .= $error[0]."\n";
+					}
+				} else {
+					$action = "add";
+				}
 			}
+			$json['errors'] = $errors;
+			$json['action'] = $action;
+			echo json_encode($json);
 		}
 
 	}
--- app/View/Employees/index.ctp
+++ app/View/Employees/index.ctp
@@ -1,123 +1,285 @@
 
 <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-
 <link href="http://handsontable.com//styles/main.css" rel="stylesheet">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <link href="http://handsontable.com//bower_components/handsontable/dist/handsontable.full.min.css" rel="stylesheet">
 <script src="http://handsontable.com//bower_components/handsontable/dist/handsontable.full.min.js"></script>
 
 <script>
 var selected_row = null;
-
+var selected_cell = null;
+var dropdownIndex = 0;
 $(document).ready(function () {
 
-	$(document).click(function(e) {
 
-		if(selected_row === null) {
-			$("#adv-example textarea").keyup(function() {
-				if(e.toElement.className === "txt-name current") {
-					$("#suggested-names").css({'display':'block',
-						'margin-top':selected_row.offsetTop+$("#suggested-names").height() + 65 + 'px',
-					  'margin-left':e.toElement.offsetLeft + 20 + 'px',});
-					$.post("employees/suggestNames",{name:$("#adv-example textarea").val()},
-						function(data) {
+	var advancedData = [];
 
-							$("#suggested-names").html(data);
-							$("#suggested-names").val("");
+	var searchValue = "";
+	var names = [];
+	var positions = [];
+	var positionLevels = [];
+	fillDropDown();
+	
+	$(document).click(function() {
 
-						});
+		if(selected_row = null) {
+			$("#table-employees texarea").blur(saveChanges);
+			$("#table-employees texarea").change(saveChanges);
+		}
+
+	});
+
+	$("#cbo-position").change(function() {
+
+		getPositionLevels();
+		searchValue = $(this).val();
+		getEmployees();
+
+	});
+
+	$("#cbo-position-level").change(function() {
+
+		getEmployees();
+
+	});
+
+	$("#cbo-status").change(function() {
+
+		searchValue = $("#cbo-status").val();
+		getEmployees();
+
+	});
+
+	function  fillDropDown() {
+
+		$.post('employees/getDropdownValues',function(data) {
+
+			names = data.names;
+			positions = data.positions;
+			positionLevels = data.positionLevels;
+
+		},'JSON');
+
+	}
+	
+	function getPositions() {
+
+		$.post("employees/getPositions",{value:""},function(data) {
+
+			$("#cbo-position").append("<option value=''> Position </option>");
+			for(var x in data) {
+				$("#cbo-position").append("<option value='" + data[x] + "'>" + data[x] + "</option>");
+			}
+			getEmployees();
+
+		});
+
+	}
+
+	function getPositionLevels() {
+
+		$.post("employees/getPositionLevels",{value:"",position:$(".cbo-position").val()},
+			function(data) {
+
+				$("#cbo-position-level").html("");
+				$("#cbo-position-level").append("<option value=''> Position Level </option>");
+				for(var x in data) {
+					$("#cbo-position-level").append("<option value='" + data[x] + "'>" + data[x] + "</option>");
 				}
 
-			});
-		}
-		var className = e.toElement.className.split(' ')[e.toElement.className.split(' ').length-1];
-		if(className === "current") {
-			selected_row = e.toElement.parentNode;
-			saveChanges();
-		}
+		},'JSON');
 
-	});
-
-
-	$("#suggested-names").change(function() {
-
-		if($("#suggested-names").val().length > 0) {
-			$("#adv-example textarea").val($("#suggested-names").val());
-			$("#suggested-names").css('display','none');
-			var index = $("tbody").children().index(selected_row);
-			selected_row.childNodes[1].innerHTML = $("#adv-example textarea").val();
-			advancedData[index]['name'] = $("#adv-example textarea").val();
-		}
-
-	});
+	}
 	
+	getPositions();
 
-	function get_employees() {
+	$("#cbo-category").change(function() {
 
-		$.post('employees/getEmployees',function(data) {
+		switch($("#cbo-category").val()) {
+			case "name" :
+				$(".cbo-position").css('display','none');
+				$("#txt-search").css('display','block');
+				$("#cbo-status").css('display','none');
+			break;
+			case "employee_id" :
+				$(".cbo-position").css('display','none');
+				$("#txt-search").css('display','block');
+				$("#cbo-status").css('display','none');
+			break;
+			case "position" :
+				$(".cbo-position").css('display','block');
+				$("#txt-search").css('display','none');
+				$("#cbo-position-level").html("");
+				$("#cbo-status").css('display','none');
+			break;
+			case "status":
+				$(".cbo-position").css('display','none');
+				$("#txt-search").css('display','none');
+				$("#cbo-status").css('display','block');
+			break;
+		}
 
-			advancedData = data;
-			display_employees();
+	});
+
+	$("#txt-search").keypress(function(e) {
+
+		if(e.keyCode === 13) {
+			searchValue = $(this).val();
+			getEmployees();
+		}
+
+	});
+
+	function getNames() {
+
+		$.post('employees/getNames',
+			function(data) {
+
+				names = data;
+				displayEmployees();
 
 		},'JSON')
 
 	}
 
-	get_employees();
+	function getEmployees() {
 
-	function display_employees() {
+		$.post('employees/getEmployees',{field:$("#cbo-category").val(),value:searchValue,
+																		position_level:$("#cbo-position-level").val()},
+			function(data) {
 
-	  var hot = new Handsontable($("#adv-example")[0], {
+				advancedData = data;
+				displayEmployees();
+
+		},'JSON')
+
+	}
+
+
+	function displayEmployees() {
+
+		$("#table-employees").html("");
+	  var hot = new Handsontable($("#table-employees")[0], {
 	    data: advancedData,
 	    height: 396,
-	    colHeaders: ["Name","Employee ID", "Tin", "Salary", "Drug Test", "Pagibig", "Philhealth", "SSS", "Insurance ID","Position","Position Level","Contract"],
+	    colHeaders: ["Name","Employee ID", "Tin", "Salary", "Drug Test", "Pagibig", "Philhealth", "Medical", "SSS", "Insurance ID","Position","Position Level","Contract","First Time in","First Time out","Last Time in","Last Time out", "Role", "Status"],
 	    rowHeaders: true,
 	    stretchH: 'all',
 	    columnSorting: true,
 	    contextMenu: true,
 	    className: "htCenter htMiddle",
 	    columns: [
-	      {data: 'name', type: 'text', className:'txt-name'},
+	   		{
+	   			data: 'name', 
+	   			type: 'autocomplete',
+	   			source: names,
+	   			strict: false
+	   		},
 		    {data: 'employee_id', type: 'text'},
-	      {data: 'tin', type: 'text'},
+	      {data: 'tin', type: 'numeric'},
 	      {data: 'salary', type: 'text'},
 	      {data: 'drug_test', type: 'text'},
 	      {data: 'pagibig', type: 'text'},
 	      {data: 'philhealth', type: 'text'},
+	      {data: 'medical', type: 'text'},
 	      {data: 'sss', type: 'text'},
 	      {data: 'insurance_id', type: 'text'},
-	      {data: 'position', type: 'text'},
-	      {data: 'position_level', type: 'text'},
-	      {data: 'contract', type: 'text'}
+	      {
+	      	data: 'position', 
+	      	type: 'dropdown',
+	      	source: positions
+	      },
+	      {
+	      	data: 'position_level', 
+	      	type: 'dropdown',
+	      	source: positionLevels
+	      },
+	      {data: 'contract', type: 'text'},
+	      {data: 'first_time_in', type: 'text'},
+	      {data: 'first_time_out', type: 'text'},
+	      {data: 'last_time_in', type: 'text'},
+	      {data: 'last_time_out', type: 'text'},
+	      {data: 'role', type: 'text'},
+	      {
+	      	data: 'status', 
+	      	type: 'dropdown',
+	      	source: ['Active', 'Inactive']
+	      }
 	    ]
 	  });
+
+		hot.addHook('afterChange',function(e) {
+
+			var index = e[0][0];
+			if(advancedData[index].name !== null) {
+				$.post('employees/saveChanges',{employee:advancedData[index]},function(data) {
+					
+					if(data.errors) {
+						//selected_cell.style.background = "rgb(208,0,0)";
+					} else {
+
+					}
+
+				},'JSON');
+			}
+
+		});
+
 	}
   
 });
 
-var advancedData = [];
-function saveChanges() {
-
-	var index = $("tbody").children().index(selected_row);
-	if(advancedData[index].name !== null) {
-		$.post('employees/saveChanges',{employee:advancedData[index]},function(data) {
-
-
-			
-		});
-	}
-
-};
-
 </script>
 <style>
-#suggested-names {
+#table-employees {
+	z-index: 0;
+}
+#autocomplete{
 	display: none;
 	position: fixed;
-	z-index: 999;
+	font: 14px "Trebuchet MS",sans-serif;
+	z-index: 9999;
 	width: 300px;
 	left: 0;
 	top: 0;
 }
+#autocomplete input{
+	opacity: 0;
+	z-index: -111;
+}
+.id-number {
+	display: none;
+}
+.cbo-position {
+	display: none;
+}
+#cbo-status {
+	display: none;
+}
+.invalid {
+	background: red;
+}
 </style>
-<div id="adv-example"></div>
-<select id="suggested-names"></select>
+
+<div id="autocomplete">
+  <input>
+</div>
+<div id="search-container">
+	<label for="txt-search"></label>
+	<select id="cbo-category">
+		<option value=""> Search By </option>
+		<option value="employee_id"> Employee ID </option>
+		<option value="name"> Name </option>
+		<option value="position"> Position </option>
+		<option value="status"> Status </option>
+	</select>
+	<input type="text" id="txt-search" placeholder="Search">
+	<select id="cbo-position" class="cbo-position"></select>
+	<select id="cbo-position-level"  class="cbo-position"></select>
+	<select id="cbo-status">
+		<option value=""> Status </option>
+		<option value="1"> Active </option>
+		<option value="0"> Inactive </option>
+	</select>
+</div>
+<div id="table-employees"></div>
