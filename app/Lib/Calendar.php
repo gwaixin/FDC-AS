65f47623a6cd05072039064560202424b6dba902
gwaixin <xin_martinez@yahoo.com>
2015-06-10 14:06:48 +0800

    Calendar new methods

--- app/Lib/Calendar.php
+++ app/Lib/Calendar.php
@@ -10,15 +10,14 @@ class Calendar {
     public $firstDay;
     public $totalDays;
     public $currentDate;
+    public $year;
 
     public function ini($currentDate = '') {
-        //date_default_timezone_set('UTC');
-        //echo date("Y-m-d H:i:s");
-        //$this->days = cal_days_in_month(CAL_GREGORIAN, $this->month, date("Y", strtotime($currentDate)));
         $currentDate = empty($currentDate) ? date('Y-m-d') : $currentDate;
 
         $this->currentDate = $currentDate;
         $this->month = date('m', strtotime($currentDate));
+        $this->year = date('Y', strtotime($currentDate));
         $this->d = new DateTime(date('Y', strtotime($this->currentDate))."-".$this->month);
         $this->today = date('j', strtotime($currentDate));
         $this->d->modify('first day of this month');
@@ -34,6 +33,34 @@ class Calendar {
             'Fri',
             'Sat'
         );
+    }
+
+    public function getMonths() {
+        $months = array(
+            '01' => 'January',
+            '02' => 'February',
+            '03' => 'March',
+            '04' => 'April',
+            '05' => 'May',
+            '06' => 'June',
+            '07' => 'July',
+            '08' => 'August',
+            '09' => 'September',
+            '10' => 'October',
+            '11' => 'November',
+            '12' => 'December' 
+        );
+        return $months;
+    }
+
+    public function getYears($start, $end = '') {
+        $yearEnd = empty($end) ? date('Y') : $end;
+        $yearStart = $start;
+        $year = array();
+        for (;$yearStart <= $yearEnd; $yearStart++) {
+            $year[] = $yearStart;
+        }
+        return $year;
     }
 }
     
